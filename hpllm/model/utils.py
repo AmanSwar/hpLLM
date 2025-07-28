985d3cf7155ba51f1e05d6e630812f70cb221e83
Aman <aman.swar001@gmail.com>
2025-07-28 17:00:40 +0530

    add ShardingRules dataclass

--- hpllm/model/utils.py
+++ hpllm/model/utils.py
@@ -2,7 +2,46 @@ import jax
 from jax import tree_util
 
 import dataclasses
+from typing import TypeAlias
 
+AxisName: TypeAlias = str | tuple[str, ...] | None
+
+
+@dataclasses.dataclass
+class ShardingRules:
+    batch: AxisName
+    sequence: AxisName
+
+    # activation
+    act_embed: AxisName
+    act_heads: AxisName
+
+    # attention
+    head_dim: AxisName
+    qkv_embed: AxisName
+    q_heads: AxisName
+    kv_heads: AxisName
+    o_heads: AxisName
+    o_embed: AxisName
+
+    # MLP layer
+    mlp_up_embed: AxisName
+    mlp_up_ffw: AxisName
+    mlp_down_ffw: AxisName
+    mlp_down_embed: AxisName
+
+    # MOE layer
+    moe_e_experts: AxisName
+    moe_e_up_embed: AxisName
+    moe_e_up_ffw: AxisName
+    moe_e_down_ffw: AxisName
+    moe_e_down_embed: AxisName
+    moe_e_tp: AxisName  # MOE forward function for Tensor Parallelism
+    moe_e_ep: AxisName  # MOE forward func for expert parallism
+
+    # vocab
+    vocab_in: AxisName
+    vocab_out: AxisName
 
 
 def pytree_struct(cls, meta_fields: tuple = ()):
@@ -12,7 +51,7 @@ def pytree_struct(cls, meta_fields: tuple = ()):
     Args:
         meta_fields (tuple, optional): meta_fields about the class , field which are supposed to be meta information. Defaults to ().
 
-    """    
+    """
     assert not dataclasses.is_dataclass(cls)
 
     cls = dataclasses.dataclass(cls)
@@ -22,5 +61,3 @@ def pytree_struct(cls, meta_fields: tuple = ()):
     return tree_util.register_dataclass(
         cls, data_fields=data_fields, meta_fields=meta_fields
     )
-
-
