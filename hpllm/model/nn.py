510dbaa2c0622eb055f24ba9c3833c36676c999d
Aman <aman.swar001@gmail.com>
2025-07-28 20:53:11 +0530

    mv helper func to util.py

--- hpllm/model/nn.py
+++ hpllm/model/nn.py
@@ -15,27 +15,4 @@ class ArrayInfo:
     metadata : dict = dataclasses.field(default_factory=dict)
 
 
-#helper functions
-is_type = lambda x , cls : (type(x).__name__ == cls.__name__) and (type(x).__module__ == cls.__module__)
 
-is_param = lambda x : is_type(x , ArrayInfo)
-
-count_left_padding = lambda ids , pad_id = 0 : auto_axes(
-    lambda segment_ids : jnp.sum(
-        jnp.cumsum(jnp.flip(segment_ids != 0 , -1) , axis=-1) > 0 , 
-        -1
-    ),
-    out_sharding=P(None),
-
-)(ids)
-
-
-lenght_minus_padding = lambda segment_ids : auto_axes (
-    lambda segment_ids : jnp.sum(
-        jnp.cumsum(jnp.flip(segment_ids != 0 , -1) , axis=-1) > 0 , -1
-    ),
-    out_sharding=P(None),
-
-)(segment_ids)
-
-which_platform = lambda cfg : cfg.mesh.devices.reshape(-1)[0].platform
