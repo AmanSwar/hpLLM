8fc3888318bd6a67bcd2e6589387458a804a8f5c
Aman <aman.swar001@gmail.com>
2025-08-01 16:13:58 +0530

    feat(model test) : add init test for MLPLayer

--- tests/model/module_tests.py
+++ tests/model/module_tests.py
@@ -2,13 +2,12 @@ import jax
 
 
 from hpllm.model.attention.attention import AttentionLayer
-# from hpllm.model.ffn.linear import MLPLayer
+from hpllm.model.ffn.linear import MLPLayer
 from tests.model.util import model_config
 
-
-
+key = jax.random.PRNGKey(0)
 def attention_init_test():
-    key = jax.random.PRNGKey(0)
+    
 
     attn_weights = AttentionLayer.init(key=key , cfg=model_config)
 
@@ -16,7 +15,13 @@ def attention_init_test():
     print(type(attn_weights))
     
 
+def mlp_init_test():
+
+    mlp_weights = MLPLayer.init(key=key , cfg=model_config)
+
+    print(mlp_weights)
+    print(type(mlp_weights))
 
 
 if __name__ == "__main__":
-    attention_init_test()
+    mlp_init_test()
