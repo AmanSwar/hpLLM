d417d72565a97e76d5904d19c399e4f7800855aa
Aman <aman.swar001@gmail.com>
2025-08-01 20:09:42 +0530

    feat(test) : add test for TransformerLayer

--- tests/model/module_tests.py
+++ tests/model/module_tests.py
@@ -1,13 +1,14 @@
 import jax
 
 
-from hpllm.model.attention.attention import AttentionLayer
+from hpllm.model.attention.attention import AttentionLayer , TransformerLayer
 from hpllm.model.ffn.linear import MLPLayer
 from hpllm.model.ffn.moe import MOELayer
 
 from tests.model.util import model_config
 
 key = jax.random.PRNGKey(0)
+
 def attention_init_test():
 
     attn_weights = AttentionLayer.init(key=key , cfg=model_config)
@@ -32,6 +33,16 @@ def moe_init_test():
     print(type(moe_weights))
 
 
+def transformerlayer_init_test():
+
+    transformer_layer_weights = TransformerLayer.init(key , cfg=model_config , layer_index=2)
+
+    print(transformer_layer_weights)
+    print(type(transformer_layer_weights))
+
+
+
 if __name__ == "__main__":
-    moe_init_test()
+    # moe_init_test()
     # mlp_init_test()
+    transformerlayer_init_test()
